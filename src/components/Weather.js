d2132366f359eed4589680735753bd5786f7c263
levan mchedlishvili <golocogo4@gmail.com>
2022-08-23 12:59:34 +0400

    add weather information and icons

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -22,7 +22,9 @@ const Weather = () => {
             inputRef.current.style.width = '30px';
         }
     }
-    console.log(weatherData);
+
+    console.log(weatherData)
+
   return (
     <section className='weather-wrapper'>
         <div className='input-wrapper'>
@@ -31,9 +33,47 @@ const Weather = () => {
             <p>,it's clear.</p>
         </div>
 
-        <div className='information-wrapper'>
+        {typeof weatherData.main !== 'undefined' ?
+                <div className='information-wrapper'>
+                <div className='icon-container'>
+                    <p>{weatherData.weather[0].main === 'Clear' ? <img className='condition-icon' src='/images/sunny.png' alt='Clear'/>
+                    :weatherData.weather[0].main === 'Rain' ? <img className='condition-icon' src='/images/raining.png' alt='Rain'/>
+                    :weatherData.weather[0].main === 'Clouds' ? <img className='condition-icon' src='/images/cloud.png' alt='Clouds'/>
+                    :weatherData.weather[0].main === 'Snow' ?<img className='condition-icon' src='/images/snow.png' alt='Snow'/>
+                    :weatherData.weather[0].main === 'Mist' ?<img className='condition-icon' src='/images/mist.png' alt='Snow'/>
+                    :
+                    <></>}</p>
+                </div>
+    
+                <div className='celsius-container'>
+                    <p>{Math.round(weatherData.main.temp - 273.15)}<span className='c'>°C</span></p>
+                </div>
 
-        </div>
+                <div className='detail-information'>
+                    <div className='detail'>
+                        <div className='detail-icon'>
+                            <img className='icon-image' src='/images/wind.png'/>
+                        </div>
+                        <p>14 <span>mph</span></p>
+                    </div>
+                    <div className='detail'>
+                        <div className='detail-icon'>
+                            <img className='icon-image' src='/images/umbrella.png'/>
+                        </div>
+                        <p>14 <span>%</span></p>
+                    </div>
+                    <div className='detail'>
+                        <div className='detail-icon'>
+                            <img className='icon-image' src='/images/humidity.png'/>
+                        </div>
+                        <p>{weatherData.main.humidity} <span>%</span></p>
+                    </div>
+                </div>
+            </div>
+            :
+            <></>        
+    }
+
 
     </section>
   )
